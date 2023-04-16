1986220923191357269565fa4966d9ef25db612c
alexstroke <busyfifer@gmail.com>
2023-04-16 15:19:00 +0400

    Initial Commit

---
+++ Shared/LearnTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTextApp.swift
+//  Shared
+//
+//  Created by Александр Строков on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
